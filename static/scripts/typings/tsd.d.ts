b52eae27aa5cab4dea11c46780c719b6c4bd960f
Yannick Cohen <yannick@everspire.com>
2016-01-25 17:00:33 -0500

    [typings] Comitted typings files (were used before) and updated gitignore

---
+++ static/scripts/typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="restangular/restangular.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
